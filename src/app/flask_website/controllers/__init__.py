b3b170477535c9af0a6434b6c5d45c51e364d5e8
nativcos <patris.morson@gmail.com>
2020-06-09 14:42:49 +0300

    features admin news and matches

--- src/app/flask_website/controllers/__init__.py
+++ src/app/flask_website/controllers/__init__.py
@@ -3,4 +3,7 @@ from .siteadmin import siteadmin_blueprints
 from .root import root_blueprints
 from .api import api_blueprints
 
+import locale
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+
 __all__ = ['contentmaker_blueprints','siteadmin_blueprints','root_blueprints','api_blueprints']
